c891d09bb26e5cec0b5edcf08f2fbe8714b7430c
ErnestBidouille <theo.chennebault@gmail.com>
2020-02-29 20:36:02 +0100

    Added ValidationRules

---
+++ src/main/java/eu/candidata/m1/project/rules/validation/ValidationRules.java
@@ -0,0 +1,14 @@
+package eu.candidata.m1.project.rules.validation;
+
+public class ValidationRules {
+
+    public static final TypeRule<Integer> IS_INTEGER = new IntegerRule();
+    private static final Rule IS_INTEGER_AGE = IS_INTEGER.then(IntegerRule::isAge);
+    public static final Rule IS_AGE = IS_INTEGER_AGE;
+    public static final TypeRule<String> IS_STRING = new StringRule();
+    public static final TypeRule<String> IS_EMAIL = new EmailRule();
+    public static final Rule IS_DAUPHINE_EMAIL = IS_EMAIL.then(EmailRule::isDauphineEmail);
+
+    private ValidationRules() {
+    }
+}
