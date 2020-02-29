4e6fc6759b111179136677cbcb31853e7006e25e
ErnestBidouille <theo.chennebault@gmail.com>
2020-02-29 20:56:56 +0100

    Added DoubleRule to ValidationRules

--- src/main/java/eu/candidata/m1/project/rules/validation/ValidationRules.java
+++ src/main/java/eu/candidata/m1/project/rules/validation/ValidationRules.java
@@ -9,7 +9,9 @@ public class ValidationRules {
 
     public static final TypeRule<Integer> IS_INTEGER = new IntegerRule();
     private static final Rule IS_INTEGER_AGE = IS_INTEGER.then(IntegerRule::isAge);
-    public static final Rule IS_AGE = IS_INTEGER_AGE;
+    public static final TypeRule<Double> IS_DOUBLE = new DoubleRule();
+    private static final Rule IS_DOUBLE_AGE = IS_DOUBLE.then(DoubleRule::isAge);
+    public static final Rule IS_AGE = IS_INTEGER_AGE.or(IS_DOUBLE_AGE);
     public static final TypeRule<String> IS_STRING = new StringRule();
     public static final TypeRule<String> IS_EMAIL = new EmailRule();
     public static final Rule IS_DAUPHINE_EMAIL = IS_EMAIL.then(EmailRule::isDauphineEmail);
