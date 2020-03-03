7cab6791e9ce57dd734fd5cb17a0acd2d68d0b9c
ErnestBidouille <theo.chennebault@gmail.com>
2020-03-04 00:56:51 +0100

    Main file system

--- src/test/java/eu/candidata/m1/project/Main.java
+++ src/test/java/eu/candidata/m1/project/Main.java
@@ -1,7 +1,18 @@
 package eu.candidata.m1.project;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.Reader;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import eu.candidata.m1.project.format.csv.CsvDataManager;
+import eu.candidata.m1.project.format.json.JsonDataAnonymizer;
+import eu.candidata.m1.project.format.json.JsonDataDescriptor;
+import eu.candidata.m1.project.format.json.JsonDataValidator;
 
 public class Main {
 
@@ -11,5 +22,6 @@ public class Main {
         if (args.length != 4) {
             throw new IllegalArgumentException("4 or 5 args needed");
         }
+        
     }
 }
